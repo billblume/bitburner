0ce8629c974d12c3695e85c1807b56152556a88c
Bill Blume <billblume@gmail.com>
2022-03-04 19:49:44 -0800

    Script for pumping you up

---
+++ src/gym-workout.ts
@@ -0,0 +1,32 @@
+import { NS } from '@ns'
+
+const GYM = 'powerhouse gym';
+
+export async function main(ns : NS) : Promise<void> {
+    while (true) {
+        const player = ns.getPlayer();
+        const str = player.strength;
+        const def = player.defense;
+        const agi = player.agility;
+        const dex = player.dexterity;
+        let stat;
+
+        if (str <= def && str <= agi && str <= dex) {
+            stat = 'strength';
+        } else if (def <= agi && def <= dex) {
+            stat = 'defense';
+        } else if (agi <= dex) {
+            stat = 'agility';
+        } else {
+            stat = 'dexterity';
+        }
+
+        const success = ns.gymWorkout(GYM, stat, true);
+
+        if (! success) {
+            return;
+        }
+
+        await ns.sleep(30000);
+    }
+}
